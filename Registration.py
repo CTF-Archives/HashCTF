58b5b6e3e3ec66cbeee71221af46c8f1aa5fec4c
Randark <randark@foxmail.com>
2024-10-01 22:09:48 +0800

    update

--- Registration.py
+++ Registration.py
@@ -7,6 +7,7 @@ from urllib.parse import quote
 if __name__ == "__main__":
     # python Registration.py ā actor ā username ā email ā biography
     user_input = [i.strip() for i in " ".join(sys.argv[1:]).split("ā")[1:]]
+    user_input = [i for i in user_input if i != ""]
     if len(user_input) != 4:
         sys.exit(1)
     actor, username, email, biography = user_input
