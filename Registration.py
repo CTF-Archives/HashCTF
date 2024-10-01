4c3625bc6341168555cab441a0c689fdfb709e99
Randark <randark@foxmail.com>
2024-10-01 21:08:36 +0800

    update

---
+++ Registration.py
@@ -0,0 +1,31 @@
+import sys
+import json
+import uuid
+from hashlib import md5
+
+
+if __name__ == "__main__":
+    # python Registration.py actor username email biography
+    if len(sys.argv) != 5:
+        sys.exit(1)
+    actor, username, email, biography = sys.argv[1:]
+    print(actor, username, email, biography)
+
+    with open("./users.json", "r") as f:
+        users_data: dict = json.load(f)
+
+    username_md5 = md5(bytes(username)).hexdigest()
+    if username_md5 in users_data.keys():
+        sys.exit(2)
+    else:
+        users_data["Users"][username_md5] = {
+            "Github-Actor": actor,
+            "Username": username,
+            "Email-Address": email,
+            "User-Biography": biography,
+        }
+
+    with open("./users.json", "w") as f:
+        f.write(json.dumps(users_data, ensure_ascii=False, indent=4))
+
+    sys.exit(0)
